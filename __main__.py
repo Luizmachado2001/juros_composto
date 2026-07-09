d4ff1a4dd431c9de298ae90924f241beebb9bfc8
desafio guanabara <luizmachado20010@gmail.com>
2026-07-09 12:34:45 -0300

    001

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 from src import Cenario, SimuladorFinanceiro
 
 def main():
-    primeiro = Cenario(1000, 30, 12)
+    primeiro = Cenario(1000, 30, 24)
     
-    SimuladorFinanceiro.tempoAteAlvo(primeiro, 100000)
+    SimuladorFinanceiro.tempoAteAlvo(primeiro, 1000000)
     calculo = SimuladorFinanceiro.calcularMontante(primeiro)
     calculo.exibirResumo()
     

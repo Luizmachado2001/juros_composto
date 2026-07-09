f6501ece6c37504e4f548992240a767b53c5e913
desafio guanabara <luizmachado20010@gmail.com>
2026-07-09 20:13:53 -0300

    adicionado grafico e melhorado algumas classes

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 from .src import Cenario, SimuladorFinanceiro
 
 def main():
-    primeiro = Cenario(1000, 1.2, 12)
+    primeiro = Cenario(100000, 1.2, 12)
     
-    SimuladorFinanceiro.tempoAteAlvo(primeiro, 10000)
+    SimuladorFinanceiro.tempoAteAlvo(primeiro, 1000000)
     calculo = SimuladorFinanceiro.calcularMontante(primeiro)
     calculo.exibirResumo()
     
